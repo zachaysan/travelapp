ff27d95d3f7759cad9839add555890d95bbe13bc
Jaco Joubert <joubert.jaco@gmail.com>
2013-04-02 17:18:26 -0400

    password recovery

---
+++ js/controllers/reset_password_controller.js
@@ -0,0 +1,38 @@
+var ResetPasswordController = Ember.Controller.extend({
+  email: null,
+  errorMessage: null,
+  isProcessing: null,
+
+  resetPassword: function() {
+    this.setProperties({
+      "errorMessage": null,
+      "isProcessing": "true"
+    });
+    that = this;
+
+    Parse.User.requestPasswordReset(this.get("email"), {
+      success: function() {
+        // Password reset request was sent successfully
+        that.setProperties({
+          "isProcessing": null,
+          "email": null
+        });
+        that.transitionToRoute("login");
+      },
+      error: function(error) {
+        that.set("isProcessing", null);
+        switch(error.code) {
+          case 204:
+            that.set("errorMessage", "We need an email address to check!");
+          break;
+
+          default:
+            that.set("errorMessage", "There is no account with that email address.");
+          break;
+        }
+      }
+    });
+  }
+});
+
+module.exports = ResetPasswordController;
